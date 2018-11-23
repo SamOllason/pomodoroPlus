4f0382b66e4c23dcbccb293a6585e8ac18e22b6a
Samuel Ollason <sam_ollason@hotmail.co.uk>
2018-11-23 07:48:43 +0000

    Initial Commit

---
+++ pomodoroPlus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pomodoroPlus
+//
+//  Created by Samuel Ollason on 23/11/2018.
+//  Copyright © 2018 Samuel Ollason. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
